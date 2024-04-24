f48aebd7a1d2872ecc5f307623d0c9f369ceda44
chaos <1585767724@qq.com>
2024-04-24 23:10:56 +0800

    提桥

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/23/abc/","2024/04/23/五天速成electron——day0/","2024/04/23/后面加入-和不加大括号有什么区别/","2024/04/23/test/","2024/04/23/常用DNS地址列表/","2024/04/23/hello-world/","2024/04/23/技术随想/"];function toRandomPost(){
+var posts=["2024/04/23/abc/","2024/04/23/hello-world/","2024/04/23/五天速成electron——day0/","2024/04/23/后面加入-和不加大括号有什么区别/","2024/04/23/常用DNS地址列表/","2024/04/23/技术随想/","2024/04/24/深入比较Shadcn与Antd设计库：从传统UI到Headless组件的全面分析/","2024/04/23/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
