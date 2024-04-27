c02ac3860a18335c2a3b0336de00d4f7d126b475
chaos <1585767724@qq.com>
2024-04-27 23:32:26 +0800

    删减多余文章

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/23/abc/","2024/04/23/hello-world/","2024/04/23/import后面加入-和不加大括号有什么区别/","2024/04/23/test/","2024/04/23/五天速成electron——day0/","2024/04/23/技术随想/","2024/04/23/常用DNS地址列表/"];function toRandomPost(){
+var posts=["2024/04/23/import后面加入-和不加大括号有什么区别/","2024/04/23/五天速成electron——day0/","2024/04/23/技术随想/","2024/04/23/常用DNS地址列表/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
