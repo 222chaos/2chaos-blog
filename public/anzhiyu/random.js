8fd4f14d247e0a076ac05e753cd7f32da752a2e7
chaos <1585767724@qq.com>
2024-04-29 23:07:01 +0800

    删除内容

--- public/anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2024/04/23/import后面加入-和不加大括号有什么区别/","2024/04/23/五天速成electron——day0/","2024/04/23/常用DNS地址列表/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
