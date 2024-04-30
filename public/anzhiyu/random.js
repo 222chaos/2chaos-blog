f8842b1df1c0fe5e5491f58678302aa78039f26a
chaos <1585767724@qq.com>
2024-04-30 23:44:55 +0800

    修改内容

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/29/commit/","2024/04/23/import后面加入-和不加大括号有什么区别/","2024/04/23/常用DNS地址列表/","2024/04/23/五天速成electron——day0/"];function toRandomPost(){
+var posts=["2024/04/23/import后面加入-和不加大括号有什么区别/","2024/04/23/五天速成electron——day0/","2024/04/23/常用DNS地址列表/","2024/04/30/浅度对比 Shadcn 与 Ant Design/","2024/04/29/规范的 git commit/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
