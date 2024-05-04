172a3df8c721f697b91dfb62c82e763f4872a11e
chaos <1585767724@qq.com>
2024-05-04 23:57:54 +0800

    1

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/23/五天速成electron——day0/","2024/05/03/什么是css变量？/","2024/04/23/常用DNS地址列表/","2024/04/29/规范的 git commit/","2024/04/30/浅度对比 Shadcn 与 Ant Design/","2024/04/23/import后面加入-和不加大括号有什么区别/"];function toRandomPost(){
+var posts=["2024/05/04/大学test/","2024/04/23/五天速成electron——day0/","2024/04/23/import后面加入-和不加大括号有什么区别/","2024/04/29/规范的 git commit/","2024/05/03/什么是css变量？/","2024/04/30/浅度对比 Shadcn 与 Ant Design/","2024/04/23/常用DNS地址列表/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
