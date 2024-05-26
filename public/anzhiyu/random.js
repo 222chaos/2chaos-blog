802e4385e1112cebfe92cba8e0c4116db38aed89
chaos <1585767724@qq.com>
2024-05-26 10:52:59 +0800

    修改icon

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/23/import后面加入-和不加大括号有什么区别/","2024/04/23/五天速成electron——day0/","2024/05/08/大学/","2024/05/03/什么是css变量？/","2024/04/23/常用DNS地址列表/","2024/04/29/规范的 git commit/","2024/04/30/浅度对比 Shadcn 与 Ant Design/","2024/05/20/git之获取远程分支（fetch5种方法）/"];function toRandomPost(){
+var posts=["2024/05/20/git之获取远程分支（fetch5种方法）/","2024/04/23/import后面加入-和不加大括号有什么区别/","2024/04/23/五天速成electron——day0/","2024/05/08/大学/","2024/05/03/什么是css变量？/","2024/04/23/常用DNS地址列表/","2024/04/30/浅度对比 Shadcn 与 Ant Design/","2024/04/29/规范的 git commit/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
